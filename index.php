a6c1601ae1f548d3cbb0965288c505c9af558e4f
solangeguzman <solangeguzmanduchen@gmail.com>
2021-08-26 00:01:26 +0200

    codice

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
     <title>php-badword</title>
 </head>
 <body>
+  
+Esercizio di oggi: PHP Badwords
+nome repo: php-badwords
+Descrizione:
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall’utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+
+
     <main>
  
      <?php
